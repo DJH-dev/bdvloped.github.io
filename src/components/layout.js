5e3828bd75cecdbf57aaac57d13715196a71da78
Jack Jack <dhaynes1203@gmail.com>
2020-03-27 14:49:44 -0500

    check updated header styles for phone size

--- src/components/layout.js
+++ src/components/layout.js
@@ -42,26 +42,32 @@ export default ({ children }) => {
                 flex: 0 1 100%;
                 grid-column: 1 / -1;
 
-                @media(max-width: 740px){
-                  text-align: center;
-                }
-                `}
+                  @media(max-width: 740px){
+                    text-align: center;
+                  }
+                `
+              }
         >
                 <Link to={`/`}>
                     <h2
-                        css={css`
+                      css={css`
                         margin-top:${rhythm(.5)};
                         margin-bottom: ${rhythm(2)};
                         display: inline-block;
                         font-style: normal;
-                        `}
+                          @media(max-width: 740px){
+                            margin: 1rem 0 1.08rem;
+                          }
+                       `
+                      }
                     >
                         bdvloped
                     </h2>
                 </Link>
                 <h1 
-                  css={css` text-align: center;`
-                  }>{data.site.siteMetadata.title}</h1>
+                  css={css` text-align: center;`}>
+                    {data.site.siteMetadata.title}
+                </h1>
         </header>
             {children}
     </div>
